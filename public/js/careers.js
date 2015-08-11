d42a318d488524975a8d679c50c5b99a558522b5
Alex Emslie <alex.emslie@appdirect.com>
2015-08-11 12:54:52 -0700

    icon layout

--- public/js/careers.js
+++ public/js/careers.js
@@ -1,7 +1,40 @@
 (function() {
   $(function() {
-    return $('.burger, .close').click(function() {
-      return $('.off-canvas, #container').toggleClass('active');
+    var emptyIframe, loadWistia, matchHeight;
+    $.get("http://info.appdirect.com/assets_new/svg/shapes.svg", function(data) {
+      var div;
+      div = document.createElement("div");
+      div.className = "svgstore";
+      div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
+      document.body.insertBefore(div, document.body.childNodes[0]);
+      return $('body').removeClass('no-svgs').addClass('svgs-loaded');
+    });
+    $('.burger, .close').click(function() {
+      return $('.off-canvas, #container, .overlay').toggleClass('active');
+    });
+    matchHeight = function() {
+      var iconWidth, teamIcon;
+      teamIcon = $('.columns.teams .one-quarter');
+      iconWidth = teamIcon.width();
+      return teamIcon.css("height", iconWidth);
+    };
+    matchHeight();
+    loadWistia = function(wistiaEmbed) {
+      return $('.video-launch, button.video-launch').click(function(e) {
+        var loadedIframe;
+        e.preventDefault();
+        wistiaEmbed = $(this).attr('title');
+        loadedIframe = $('.iframe-container').html('<iframe src="//fast.wistia.net/embed/iframe/' + wistiaEmbed + '?videoFoam="true" allowtransparency="true" frameborder="0" scrolling="no" id="wistia_video" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="1920" height="1080"></iframe>');
+        $(loadedIframe).appendTo('.iframe-container');
+        return $('.video-modal').modal();
+      });
+    };
+    loadWistia();
+    emptyIframe = function() {
+      return $('.iframe-container').empty();
+    };
+    return $('span.close').click(function() {
+      return emptyIframe();
     });
   });
 
--- src/coffee/careers.coffee
+++ src/coffee/careers.coffee
@@ -1,3 +1,33 @@
 $ ->
+	$.get "http://info.appdirect.com/assets_new/svg/shapes.svg", (data) ->
+		div = document.createElement("div")
+		div.className = "svgstore"
+		div.innerHTML = new XMLSerializer().serializeToString(data.documentElement)
+		document.body.insertBefore(div, document.body.childNodes[0])
+		$('body').removeClass('no-svgs').addClass('svgs-loaded')
+
 	$('.burger, .close').click ->
-		$('.off-canvas, #container').toggleClass('active')
+		$('.off-canvas, #container, .overlay').toggleClass('active')
+
+	matchHeight = ->
+		teamIcon = $('.columns.teams .one-quarter')
+		iconWidth = teamIcon.width()
+		teamIcon.css "height" , iconWidth
+	matchHeight()
+
+	loadWistia = (wistiaEmbed) ->
+    $('.video-launch, button.video-launch').click (e) ->
+      e.preventDefault()
+      wistiaEmbed = $(this).attr('title')
+      loadedIframe = $('.iframe-container').html('<iframe src="//fast.wistia.net/embed/iframe/' + wistiaEmbed + '?videoFoam="true" allowtransparency="true" frameborder="0" scrolling="no" id="wistia_video" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="1920" height="1080"></iframe>')
+      $(loadedIframe).appendTo('.iframe-container')
+      $('.video-modal').modal()
+      # $('.iframe-container iframe').on "load", ->
+      #   $('.iframe-placeholder').css "opacity" , "0"
+  loadWistia()
+
+  emptyIframe = ->
+    $('.iframe-container').empty()
+
+  $('span.close').click ->
+    emptyIframe()
